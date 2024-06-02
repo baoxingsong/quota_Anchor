3b41eb81019dba59e69d46c888ddc61a7249a5f9
xiaodli <2360669642@qq.com>
2024-06-02 13:08:25 +0800

    initial os zm sb and main->quota_Anchor

--- main.py
+++ main.py
@@ -2,81 +2,85 @@ import configparser
 import argparse
 from pathlib import Path
 import os
-from lib import pre_collinearity, collinearity, dotplot, prepare_ks, ks, blockinfo, ks_peaks, peaksfit, ksfigure, \
-     classification_gene, orthogroup3, number_gn_visualization
+from .lib import pre_collinearity, collinearity, dotplot, prepare_ks, ks, blockinfo, ks_peaks, peaksfit, ksfigure, \
+     classification_gene, orthogroup3, number_gn_visualization, orthogroup4
 
 
 base_dir = Path(__file__).resolve().parent
 
+# TODO
+# software path
 
-def run_pre_coll():
+
+def run_pre_coll(parameter):
     global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, "config_file/pre_collinearity.conf"))
+    # config_par.read(os.path.join(base_dir, "config_file/pre_collinearity.conf"))
+    config_par.read(parameter.conf)
     config_soft = configparser.ConfigParser()
     config_soft.read(os.path.join(base_dir, 'config_file/software_path.ini'))
     pre_collinearity.Prepare(config_par, config_soft).run_all_process()
 
 
-def run_coll():
+def run_coll(parameter):
     global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/collinearity.conf'))
+    config_par.read(parameter.conf)
     config_soft = configparser.ConfigParser()
     config_soft.read(os.path.join(base_dir, 'config_file/software_path.ini'))
     collinearity.Collinearity(config_par, config_soft).run()
 
 
-def run_dotplot():
-    global base_dir
+def run_dotplot(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/dotplot.conf'))
+    config_par.read(parameter.conf)
     dotplot.Dotplot(config_par).run()
 
 
-def run_prepare_ks():
-    global base_dir
+def run_prepare_ks(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/ks.conf'))
+    config_par.read(parameter.conf)
     config_soft = configparser.ConfigParser()
-    config_soft.read(os.path.join(base_dir, 'config_file/software_path.ini'))
+    config_soft.read(parameter.conf)
     prepare_ks.Prepare(config_par, config_soft).run()
 
 
-def run_ks():
-    global base_dir
+def run_ks(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/ks.conf'))
+    config_par.read(parameter.conf)
     config_soft = configparser.ConfigParser()
-    config_soft.read(os.path.join(base_dir, 'config_file/software_path.ini'))
+    config_soft.read(parameter.conf)
     ks.Ks(config_par, config_soft).sub_run()
 
 
-def run_block_info():
-    global base_dir
+def run_block_info(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/block_info.conf'))
+    config_par.read(parameter.conf)
     blockinfo.BlockInfo(config_par).run()
 
 
-def run_ks_pdf():
-    global base_dir
+def run_ks_pdf(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/ks_peaks.conf'))
+    config_par.read(parameter.conf)
     ks_peaks.KsPeaks(config_par).run()
 
 
-def run_pdf_fit():
-    global base_dir
+def run_pdf_fit(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/peaks_fit.conf'))
+    config_par.read(parameter.conf)
     peaksfit.PeaksFit(config_par).run()
 
 
-def run_ks_figure():
-    global base_dir
+def run_ks_figure(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/ks_figure.conf'))
+    config_par.read(parameter.conf)
     ksfigure.KsFigure(config_par).run()
 
 
@@ -92,10 +96,10 @@ def run_ks_figure():
 #     classification.Classification(config_par).run()
 
 
-def run_class_gene():
-    global base_dir
+def run_class_gene(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/classification_gene.conf'))
+    config_par.read(parameter.conf)
     if int(config_par["classification"]["type"]) == 1:
         classification_gene.ClassGeneUnique(config_par).run()
     else:
@@ -116,17 +120,24 @@ def run_class_gene():
 #     orthogroup2.Group(config_par).run()
 
 
-def run_group3():
-    global base_dir
+def run_group3(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/orthogroup3.conf'))
+    config_par.read(parameter.conf)
     orthogroup3.Group(config_par).run()
 
 
-def run_clv():
-    global base_dir
+def run_group4(parameter):
+    # global base_dir
     config_par = configparser.ConfigParser()
-    config_par.read(os.path.join(base_dir, 'config_file/class_gene_number_visualization.conf'))
+    config_par.read(parameter.conf)
+    orthogroup4.Group(config_par).run()
+
+
+def run_clv(parameter):
+    # global base_dir
+    config_par = configparser.ConfigParser()
+    config_par.read(parameter.conf)
     number_gn_visualization.ClsVis(config_par).run()
 # def run_class_gene_2():
 #     config_par = configparser.ConfigParser()
@@ -137,60 +148,77 @@ def run_clv():
 #         classification_gene_2.ClassGene(config_par).run()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='collinearity gene analysis', prog="quotaAnchor")
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1_053024_beta')
-    subparsers1 = parser.add_subparsers(title='gene collinearity analysis', dest='analysis')
+parser = argparse.ArgumentParser(description='collinearity gene analysis', prog="quota_Anchor")
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1_053024_beta')
 
-    # get the longest protein and AnchorWave pro input file
-    parser_sub1 = subparsers1.add_parser('pre_col', help='get longest protein and AnchorWave pro(collinearity) input file')
-    parser_sub1.set_defaults(func=run_pre_coll)
-    # produce collinearity file
-    parser_sub1 = subparsers1.add_parser('col', help='get gene collinearity file by AnchorWave pro command')
-    parser_sub1.set_defaults(func=run_coll)
-    # collinearity dotplot or blast dotplot
-    parser_sub1 = subparsers1.add_parser('dotplot', help='collinearity dotplot or blast dotplot')
-    parser_sub1.set_defaults(func=run_dotplot)
-    # prepare data for synonymous mutation and non-synonymous mutation(longest cds)
-    parser_sub1 = subparsers1.add_parser('pre_ks', help='get longest cds')
-    parser_sub1.set_defaults(func=run_prepare_ks)
-    # synonymous mutation and non-synonymous mutation
-    parser_sub1 = subparsers1.add_parser('ks', help='get ks and ka information')
-    parser_sub1.set_defaults(func=run_ks)
-    # summary block information
-    parser_sub1 = subparsers1.add_parser('block_info', help='summary block information')
-    parser_sub1.set_defaults(func=run_block_info)
-    # ks probability density function curve
-    parser_sub1 = subparsers1.add_parser('kp', help='ks(total, average, median) probability density function curve')
-    parser_sub1.set_defaults(func=run_ks_pdf)
-    # ks probability density function curve fitting
-    parser_sub1 = subparsers1.add_parser('pf', help='ks probability density function curve fitting')
-    parser_sub1.set_defaults(func=run_pdf_fit)
-    # ks probability density function curve fitting
-    parser_sub1 = subparsers1.add_parser('kf', help='ks distribution figure')
-    parser_sub1.set_defaults(func=run_ks_figure)
-    # #
-    # parser_sub1 = subparsers1.add_parser('get_chr_info', help='assumed ancestor file')
-    # parser_sub1.set_defaults(func=run_pre_classification)
-    # # get ancestor chr 1 chr_total_gene_number class
-    # parser_sub1 = subparsers1.add_parser('class', help='relative classification')
-    # parser_sub1.set_defaults(func=run_classification)
-    # class gene
-    parser_sub1 = subparsers1.add_parser('class_gene', help='class gene as tandem, proximal, transposed, wgd/segmental, dispersed, singletons')
-    parser_sub1.set_defaults(func=run_class_gene)
-    # parser_sub1 = subparsers1.add_parser('group', help='orthogroup based collinearity')
-    # parser_sub1.set_defaults(func=run_group)
-    # parser_sub1 = subparsers1.add_parser('group2', help='orthogroup based collinearity')
-    # parser_sub1.set_defaults(func=run_group2)
-    parser_sub1 = subparsers1.add_parser('group3', help='orthogroup based collinearity')
-    parser_sub1.set_defaults(func=run_group3)
-    parser_sub1 = subparsers1.add_parser('clv', help='class gene number visualization')
-    parser_sub1.set_defaults(func=run_clv)
+subparsers = parser.add_subparsers(title='gene collinearity analysis', dest='analysis')
+# get the longest protein and AnchorWave pro input file
+parser_sub_a = subparsers.add_parser('pre_col', help='get longest protein and AnchorWave pro(collinearity) input file')
+parser_sub_a.set_defaults(func=run_pre_coll)
+parser_sub_a.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# produce collinearity file
+parser_sub_b = subparsers.add_parser('col', help='get gene collinearity file by AnchorWave pro command')
+parser_sub_b.set_defaults(func=run_coll)
+parser_sub_b.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# collinearity dotplot or blast dotplot
+parser_sub_c = subparsers.add_parser('dotplot', help='collinearity dotplot or blast dotplot')
+parser_sub_c.set_defaults(func=run_dotplot)
+parser_sub_c.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# prepare data for synonymous mutation and non-synonymous mutation(longest cds)
+parser_sub_d = subparsers.add_parser('pre_ks', help='get longest cds')
+parser_sub_d.set_defaults(func=run_prepare_ks)
+parser_sub_d.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# synonymous mutation and non-synonymous mutation
+parser_sub_e = subparsers.add_parser('ks', help='get ks and ka information')
+parser_sub_e.set_defaults(func=run_ks)
+parser_sub_e.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# summary block information
+parser_sub_f = subparsers.add_parser('block_info', help='summary block information')
+parser_sub_f.set_defaults(func=run_block_info)
+parser_sub_f.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# ks probability density function curve
+parser_sub_g = subparsers.add_parser('kp', help='ks(total, average, median) probability density function curve')
+parser_sub_g.set_defaults(func=run_ks_pdf)
+parser_sub_g.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# ks probability density function curve fitting
+parser_sub_h = subparsers.add_parser('pf', help='ks probability density function curve fitting')
+parser_sub_h.set_defaults(func=run_pdf_fit)
+parser_sub_h.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# ks probability density function curve fitting
+parser_sub_i = subparsers.add_parser('kf', help='ks distribution figure')
+parser_sub_i.set_defaults(func=run_ks_figure)
+parser_sub_i.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# #
+# parser_sub1 = subparsers1.add_parser('get_chr_info', help='assumed ancestor file')
+# parser_sub1.set_defaults(func=run_pre_classification)
+# # get ancestor chr 1 chr_total_gene_number class
+# parser_sub1 = subparsers1.add_parser('class', help='relative classification')
+# parser_sub1.set_defaults(func=run_classification)
+# class gene
+parser_sub_j = subparsers.add_parser('class_gene', help='class gene as tandem, proximal, transposed, wgd/segmental, dispersed, singletons')
+parser_sub_j.set_defaults(func=run_class_gene)
+parser_sub_j.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+# parser_sub1 = subparsers1.add_parser('group', help='orthogroup based collinearity')
+# parser_sub1.set_defaults(func=run_group)
+# parser_sub1 = subparsers1.add_parser('group2', help='orthogroup based collinearity')
+# parser_sub1.set_defaults(func=run_group2)
+parser_sub_k = subparsers.add_parser('group3', help='orthogroup based collinearity')
+parser_sub_k.set_defaults(func=run_group3)
+parser_sub_k.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_l = subparsers.add_parser('group4', help='orthogroup based collinearity')
+parser_sub_l.set_defaults(func=run_group4)
+parser_sub_l.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_m = subparsers.add_parser('clv', help='class gene number visualization')
+parser_sub_m.set_defaults(func=run_clv)
+parser_sub_m.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
 #    parser_sub1 = subparsers1.add_parser('class_gene_2', help='class gene as tandem, proximal, transposed, wgd/segmental, dispersed, singletons')
 #    parser_sub1.set_defaults(func=run_class_gene_2)
 
-    args = parser.parse_args()
+args = parser.parse_args()
+
+
+def main():
     if hasattr(args, 'func'):
-        args.func()
+        args.func(args)
     else:
         parser.print_help()
