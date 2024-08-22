fa63c821003e65de4947ed22821ee66710be0f61
xiaodli <2360669642@qq.com>
2024-08-22 12:32:26 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -20,10 +20,7 @@ setup(
     long_description=long_des,
     long_description_content_type="text/markdown",
     url="https://github.com/baoxingsong/quota_Anchor",
-    packages=['quota_anchor', 'quota_anchor.lib', 'quota_anchor.config_file'],
-    package_data={
-        'quota_anchor': ['plots/*'],
-    },
+    packages=['quota_anchor', 'quota_anchor.lib', 'quota_anchor.config_file', 'quota_anchor.plots'],
     include_package_data=True,
     python_requires=">=3, <4",
     classifiers=[
