863ee765de0b230fb55273a32a80110e7681767c
xiaodli <143861428+xiaodli@users.noreply.github.com>
2024-08-23 16:58:22 +0800

    modify some detail

--- quota_anchor/main.py
+++ quota_anchor/main.py
@@ -65,40 +65,40 @@ def run_line_3(parameter):
     line_proali_pangenome.Line(config_par).run()
 
 
-parser = argparse.ArgumentParser(description='collinearity gene analysis', prog="quota_Anchor")
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1.0')
+parser = argparse.ArgumentParser(description='Conduct strand and WGD aware syntenic gene identification for a pair of genomes using the longest path algorithm implemented in AnchorWave', prog="quota_Anchor")
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1a0')
 
 
-subparsers = parser.add_subparsers(title='gene collinearity analysis', dest='analysis')
+subparsers = parser.add_subparsers(title='Gene collinearity analysis', dest='analysis')
 # get the longest protein and AnchorWave pro input file
-parser_sub_a = subparsers.add_parser('pre_col', help='get longest protein and AnchorWave pro(collinearity) input file')
+parser_sub_a = subparsers.add_parser('pre_col', help='Get longest protein file from gffread result and input file for gene collinearity analysis')
 parser_sub_a.set_defaults(func=run_pre_coll)
-parser_sub_a.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_a.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 # produce collinearity file
-parser_sub_b = subparsers.add_parser('col', help='get gene collinearity file by AnchorWave pro command')
+parser_sub_b = subparsers.add_parser('col', help='Get gene collinearity result file')
 parser_sub_b.set_defaults(func=run_coll)
-parser_sub_b.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_b.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 
 
-parser_sub_p_pre = subparsers.add_parser('get_chr_length', help='chromosome length and name info from fai file')
+parser_sub_p_pre = subparsers.add_parser('get_chr_length', help='Get chromosome length and name info from fai file')
 parser_sub_p_pre.set_defaults(func=run_lens)
-parser_sub_p_pre.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_p_pre.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 # collinearity dotplot or blast dotplot
-parser_sub_c = subparsers.add_parser('dotplot', help='collinearity dotplot or blast dotplot')
+parser_sub_c = subparsers.add_parser('dotplot', help='Get collinearity dotplot or blast dotplot')
 parser_sub_c.set_defaults(func=run_dotplot)
-parser_sub_c.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_c.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 # collinearity circle plot
-parser_sub_o = subparsers.add_parser('circle', help='AnchorWave pro command collinearity visualization')
+parser_sub_o = subparsers.add_parser('circle', help='Collinearity result visualization')
 parser_sub_o.set_defaults(func=run_circle)
-parser_sub_o.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_o.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 # collinearity line plot
-parser_sub_p_2 = subparsers.add_parser('line_2', help='AnchorWave pro command collinearity visualization')
+parser_sub_p_2 = subparsers.add_parser('line_2', help='Collinearity visualization')
 parser_sub_p_2.set_defaults(func=run_line_2)
-parser_sub_p_2.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_p_2.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 # collinearity AnchorWave proali anchors plot
-parser_sub_p_3 = subparsers.add_parser('line_proali', help='AnchorWave proali command pangenome collinearity visualization')
+parser_sub_p_3 = subparsers.add_parser('line_proali', help='Anchors file from AnchorWave proali visualization')
 parser_sub_p_3.set_defaults(func=run_line_3)
-parser_sub_p_3.add_argument('-c', '--conf', dest='conf', help="command configure file", metavar="")
+parser_sub_p_3.add_argument('-c', '--conf', dest='conf', help="configure file", metavar="")
 
 
 args = parser.parse_args()
