f7040f03f76d3309893607af210f8831090f73d3
xiaodli <2360669642@qq.com>
2025-05-15 00:08:44 +0800

    more robust ks.py and update readme maize genome

--- quota_anchor/lib/ks.py
+++ quota_anchor/lib/ks.py
@@ -60,6 +60,7 @@ class Ks:
         self.process = 6
         self.add_ks = False
 
+        self.debug=""
         self.cds_file = ""
         self.pep_file = ""
         self.collinearity = ""
@@ -99,71 +100,162 @@ class Ks:
             file_handler.close()
         output_file.close()
 
+    def merge_debug_file(self):
+        debug_file = open(self.debug, 'w')
+        for i in range(int(self.process)):
+            path = os.path.join(self.work_dir, 'tmp', 'process_' + str(i), os.path.basename(self.ks_file) + '_' + str(i) + "_debug.txt")
+            file_handler = open(path, 'r')
+            content = file_handler.read()
+            debug_file.write(content)
+            file_handler.close()
+        debug_file.close()
 
-    def pair_kaks(self):
+    @staticmethod
+    def output_bug_pipe(pair, error, debug_sub_file):
+        if os.path.exists(debug_sub_file):
+            debug_sub_handle = open(debug_sub_file, 'a')
+        else:
+            debug_sub_handle = open(debug_sub_file, 'w')
+        if hasattr(error, 'stderr') and hasattr(error, 'stdout'):
+            debug_sub_handle.write(f"An error occurred when processing {pair}, exception: {error.stderr}{error.stdout}\n")
+        elif hasattr(error, 'stderr'):
+            debug_sub_handle.write(f"An error occurred when processing {pair}, exception: {error.stderr}\n")
+        elif hasattr(error, 'stdout'):
+            debug_sub_handle.write(f"An error occurred when processing {pair}, exception: {error.stdout}\n")
+        else:
+            debug_sub_handle.write(f"An error occurred when processing {pair}, exception: {error}\n")
+        debug_sub_handle.close()
+
+    @staticmethod
+    def process_ng86_new_line(new_line, ng86):
+        line_floats_res = re.findall(r"-*\d+\.\d+", new_line)
+        line_floats = [float(val) for val in line_floats_res]
+        for i in range(0, len(line_floats), 3):
+            ng86["omega"] = line_floats[i]
+            ng86["dN"] = line_floats[i + 1]
+            ng86["dS"] = line_floats[i + 2]
+        ng86_flag = True
+        return ng86, ng86_flag
+
+    def process_ng86(self, sub_content, gene1, gene2, ng86, ng86_flag):
+        for line in sub_content:
+            if line.startswith(f"{gene1}") and line.startswith(f"{gene2}"):
+                gene1_name_length = len(gene1)
+                gene2_name_length = len(gene2)
+                max_gene_name_length = max(gene1_name_length, gene2_name_length)
+                new_line = line[max_gene_name_length:]
+                if not new_line.strip():
+                    continue
+                else:
+                    ng86, ng86_flag = self.process_ng86_new_line(new_line, ng86)
+                    break
+            elif line.startswith(f"{gene1}"):
+                new_line = line[len(gene1):]
+                if not new_line.strip():
+                    continue
+                else:
+                    ng86, ng86_flag = self.process_ng86_new_line(new_line, ng86)
+                    break
+            elif line.startswith(f"{gene2}"):
+                new_line = line[len(gene2):]
+                if not new_line.strip():
+                    continue
+                else:
+                    ng86, ng86_flag = self.process_ng86_new_line(new_line, ng86)
+                    break
+            else:
+                continue
+        return ng86, ng86_flag
+
+    def write_to_pair_file(self, pep, cds, k):
+        SeqIO.write([cds[k[0]], cds[k[1]]], self.pair_cds_file, "fasta")
+        SeqIO.write([pep[k[0]], pep[k[1]]], self.pair_pep_file, "fasta")
+
+    @staticmethod
+    def write_to_sub_ks_file(ka_ks, process_sub_file, k):
+        if os.path.exists(process_sub_file):
+            process_sub_handle = open(process_sub_file, 'a')
+        else:
+            process_sub_handle = open(process_sub_file, 'w')
+        ka_ks_list = [ka_ks[0]["dN"], ka_ks[0]["dS"], ka_ks[0]["omega"],
+                     ka_ks[1]["dN"], ka_ks[1]["dS"], ka_ks[1]["omega"], ka_ks[1]["t"]]
+        row_list = [str(i) for i in list(k) + ka_ks_list]
+        if len(row_list) == 9:
+            process_sub_handle.write('\t'.join(row_list) + '\n')
+        process_sub_handle.close()
+
+    def pair_kaks(self, debug_sub_file, pair):
         self.align()
         pal = self.run_pal2nal()
         if not pal:
             return []
         self.run_yn00()
-        ng86, yn00 = self.read_yn00_result()
+        ng86, yn00 = self.read_yn00_result(debug_sub_file, pair)
         ka_ks = [ng86, yn00]
         return ka_ks
 
     def align(self):
         if self.align_software == 'mafft':
             command_line = [self.mafft, "--quiet", "--auto", self.pair_pep_file]
-            try:
-                with open(self.prot_align_file, 'w') as output_file:
-                    subprocess.run(command_line, stdout=output_file, stderr=subprocess.DEVNULL, check=True)
-            except subprocess.CalledProcessError:
-                pass
+            with open(self.prot_align_file, 'w') as output_file:
+                subprocess.run(command_line, stdout=output_file, stderr=subprocess.PIPE, check=True, text=True)
         if self.align_software == 'muscle':
             command_line = [self.muscle, '-align', self.pair_pep_file, '-output', self.prot_align_file]
-            try:
-                subprocess.run(command_line, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            except subprocess.CalledProcessError:
-                pass
+            subprocess.run(command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     def run_pal2nal(self):
         command_line = [self.pal2nal, self.prot_align_file, self.pair_cds_file, '-output', 'paml', '-nogap']
-        try:
-            with open(self.pair_changed_seq, 'w') as output_file:
-                subprocess.run(command_line, stdout=output_file, stderr=subprocess.DEVNULL, check=True)
-        except subprocess.CalledProcessError:
-            return None
+        with open(self.pair_changed_seq, 'w') as output_file:
+            subprocess.run(command_line, stdout=output_file, stderr=subprocess.PIPE, check=True, text=True)
         return True
 
     def run_yn00(self):
         command_line = [self.yn00]
-        try:
-            subprocess.run(command_line, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=True)
-        except subprocess.CalledProcessError:
-            pass
+        subprocess.run(command_line, stderr=subprocess.PIPE, stdout=subprocess.PIPE, check=True, text=True)
 
-    def read_yn00_result(self):
+    def read_yn00_result(self, debug_sub_file, pair):
+        gene1 = pair[0]
+        gene2 = pair[1]
         ng86 = dict()
         yn00 = dict()
+        if os.path.exists(debug_sub_file):
+            debug_sub_handle = open(debug_sub_file, 'a')
+        else:
+            debug_sub_handle = open(debug_sub_file, 'w')
+
         with open(self.pair_yn) as f:
             content = f.readlines()
             for line in content:
                 if "(A) Nei-Gojobori (1986) method" in line:
                     a_index = content.index(line)
+                if "(B) Yang & Nielsen (2000) method" in line:
+                    b_index = content.index(line)
                 if "(C) LWL85, LPB93 & LWLm methods" in line:
                     c_index = content.index(line)
-            sub_content = content[a_index:c_index]
-            for line in sub_content:
-                # this regex is from biopython.
-                matrix_row_res = re.match(r"^(\S+?)(\s+-?\d+\.\d+.*$|\s*$|-1.0000\s*\(.*$)", line)
-                if matrix_row_res is not None:
-                    line_floats_res = re.findall(r"-*\d+\.\d+", matrix_row_res.group(2))
-                    line_floats = [float(val) for val in line_floats_res]
-                    for i in range(0, len(line_floats), 3):
-                        ng86["omega"] = line_floats[i]
-                        ng86["dN"] = line_floats[i+1]
-                        ng86["dS"] = line_floats[i+2]
-                    continue
+            sub_content_ng86 = content[a_index:b_index]
+            sub_content_yn00 = content[b_index:c_index]
 
+            ng86_flag = False
+            for line in sub_content_ng86:
+                if line.startswith(f"{gene1}") or line.startswith(f"{gene2}"):
+                    matrix_row_res = re.match(r"^(\S+?)(\s+-?\d+\.\d+.*$|\s*$|-1.0000\s*\(.*$)", line)
+                    if matrix_row_res is not None:
+                        line_floats_res = re.findall(r"-*\d+\.\d+", matrix_row_res.group(2))
+                        line_floats = [float(val) for val in line_floats_res]
+                        if not line_floats:
+                            continue
+                        else:
+                            for i in range(0, len(line_floats), 3):
+                                ng86["omega"] = line_floats[i]
+                                ng86["dN"] = line_floats[i+1]
+                                ng86["dS"] = line_floats[i+2]
+                            ng86_flag = True
+                            break
+
+            if not ng86_flag:
+                ng86, ng86_flag = self.process_ng86(sub_content_ng86, gene1, gene2, ng86, ng86_flag)
+
+            for line in sub_content_yn00:
                 row_res = re.match(r"\s+(\d+)\s+(\d+)", line)
                 if row_res is not None:
                     line_floats_res = re.findall(r"-*\d+\.\d+", line)
@@ -173,11 +265,21 @@ class Ks:
                     yn00["omega"] = line_floats[4]
                     yn00["dN"] = line_floats[5]
                     yn00["dS"] = line_floats[7]
+                    break
+            if not ng86_flag:
+                debug_sub_handle.write(f"The following is part of the result file of the yn00 program for {pair}" + "\n")
+                for ln in sub_content_ng86:
+                    debug_sub_handle.write(ln)
+                for ln in sub_content_yn00:
+                    debug_sub_handle.write(ln)
+                debug_sub_handle.close()
         return ng86, yn00
 
     def get_thread(self):
         cpu_number = cpu_count()
         thread_number = min([self.process, cpu_number])
+        if thread_number == 0:
+            thread_number = 1
         setattr(self, 'process', thread_number)
     
     def output_file_parentdir_exist(self, path, overwrite):
@@ -245,41 +347,48 @@ class Ks:
 
     def secondary_layer_run(self, pairs, i, cds, pep):
         os.chdir(os.path.join(self.work_dir, 'tmp', 'process_' + str(i)))
+        debug_sub_file = os.path.basename(self.ks_file) + "_" + str(i) + "_debug.txt"
         process_sub_file = os.path.basename(self.ks_file) + "_" + str(i)
-        process_sub_handle = open(process_sub_file, 'w')
+
         with open(self.yn00_ctl, 'w') as ctl:
             ctl.write("seqfile = " + self.pair_changed_seq + "\n" +
                       "outfile = " + self.pair_yn + "\n" +
                       "verbose = 1" + "\n" +
                       "icode = 0" + "\n" +
                       "weighting = 0" + "\n" +
-                      "commonf3x4 = 0")
+                      "commonf3x4 = 0" + "\n")
         if i == int(self.process) - 1:
             dt = datetime.datetime.now()
             time_now = dt.strftime('%Y/%m/%d %H:%M:%S')
             with alive_bar(len(pairs), title=f"[{time_now} INFO]", bar="bubbles", spinner="waves") as bar:
                 for k in pairs:
-                    SeqIO.write([cds[k[0]], cds[k[1]]], self.pair_cds_file, "fasta")
-                    SeqIO.write([pep[k[0]], pep[k[1]]], self.pair_pep_file, "fasta")
-                    kaks = self.pair_kaks()
-                    if not kaks:
-                        continue
-                    kaks_list = [kaks[0]["dN"], kaks[0]["dS"], kaks[0]["omega"],
-                                  kaks[1]["dN"], kaks[1]["dS"], kaks[1]["omega"], kaks[1]["t"]]
-                    process_sub_handle.write('\t'.join([str(i) for i in list(k)+kaks_list])+'\n')
-                    bar()
-                process_sub_handle.close()
+                    try:
+                        self.write_to_pair_file(pep, cds, k)
+                        ka_ks = self.pair_kaks(debug_sub_file, k)
+                        if not ka_ks:
+                            bar()
+                            continue
+                        self.write_to_sub_ks_file(ka_ks, process_sub_file, k)
+                        bar()
+                    except Exception as e:
+                        self.output_bug_pipe(k, e, debug_sub_file)
+                        if not os.path.exists(process_sub_file):
+                            with open(process_sub_file, 'w'):
+                                pass
+                        bar()
         else:
             for k in pairs:
-                SeqIO.write([cds[k[0]], cds[k[1]]], self.pair_cds_file, "fasta")
-                SeqIO.write([pep[k[0]], pep[k[1]]], self.pair_pep_file, "fasta")
-                kaks = self.pair_kaks()
-                if not kaks:
-                    continue
-                kaks_list = [kaks[0]["dN"], kaks[0]["dS"], kaks[0]["omega"],
-                                  kaks[1]["dN"], kaks[1]["dS"], kaks[1]["omega"], kaks[1]["t"]]
-                process_sub_handle.write('\t'.join([str(i) for i in list(k)+kaks_list])+'\n')
-            process_sub_handle.close()
+                try:
+                    self.write_to_pair_file(pep, cds, k)
+                    ka_ks = self.pair_kaks(debug_sub_file, k)
+                    if not ka_ks:
+                        continue
+                    self.write_to_sub_ks_file(ka_ks, process_sub_file, k)
+                except Exception as e:
+                    self.output_bug_pipe(k, e, debug_sub_file)
+                    if not os.path.exists(process_sub_file):
+                        with open(process_sub_file, 'w'):
+                            pass
 
     def first_layer_run(self):
         logger.info("ks module init and the following parameters are config information.")
@@ -311,6 +420,8 @@ class Ks:
             if len(pairs) == 0:
                 logger.info("No additional syntenic pairs are required to calculate ks.")
                 sys.exit(0)
+        if len(pairs) < self.process:
+            self.process = len(pairs)
         n = int(np.ceil(len(pairs) / self.process))
         os.chdir(os.path.join(self.work_dir, 'tmp'))
         pool = Pool(self.process)
@@ -326,5 +437,7 @@ class Ks:
         logger.info(f'Combine the ks files generated by each kernel into {self.ks_file}.')
         os.chdir(self.work_dir)
         self.merge_ks_file()
+        if self.debug:
+            self.merge_debug_file()
         shutil.rmtree(os.path.join(self.work_dir, 'tmp'))
         logger.info(f"Generate {self.ks_file} done!")
